d1a051dbdba1527602bf647e88042f6adfa70570
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-13 18:49:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,36 @@
-game_hash => {
+def game_hash(basketball)
+  
+ basketball => {
   {:home =>
-  [:team_name],
-  [:colors], 
-  [:players]
+  {:team_name => "name"},
+  {:colors => [color1, color2 ]}, 
+  {:players => [
+    {:player_name =>},
+    {:number =>},
+    {:shoe =>},
+    {:points =>},
+    {:rebounds =>},
+    {:assists =>},
+    {:steals =>},
+    {:blocks =>},
+    {:slam_dunks =>},
+    ]
+    }
   }
   {:away =>
-  [:team_name],
-  [:colors],
-  [:players]
+  {:team_name},
+  {:colors},
+  {:players => [
+    {:player_name =>},
+    {:number =>},
+    {:shoe =>},
+    {:points =>},
+    {:rebounds =>},
+    {:assists =>},
+    {:steals =>},
+    {:blocks =>},
+    {:slam_dunks =>},
+    ]}
   }
 }
+end
